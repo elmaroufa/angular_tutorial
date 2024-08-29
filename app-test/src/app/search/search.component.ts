afbf32c2cf265d1939ccb105fe8e09ab34ad1dfe
elmaroufa <salyabbo@gmail.com>
2024-08-29 11:20:49 +0100

    ad unit test for form

--- app-test/src/app/search/search.component.ts
+++ app-test/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+
+  get searchText() {
+    return this.searchForm.controls.searchText;
+  }
+
+  searchForm = new FormGroup({
+    searchText: new FormControl('', Validators.required)
+  });
+
+  search() {
+    if(this.searchForm.valid) {
+    console.log('You searched for: ' + this.searchText.value)
+    }
+  }
 
 }
