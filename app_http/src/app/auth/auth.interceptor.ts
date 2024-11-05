0314c7c8cdfc08d7392b36ac1eb008adaca2f4e5
elmaroufa <salyabbo@gmail.com>
2024-11-05 17:20:58 +0100

    add return observable in interceptor and inject auth service'

--- app_http/src/app/auth/auth.interceptor.ts
+++ app_http/src/app/auth/auth.interceptor.ts
@@ -1,7 +1,12 @@
 import { HttpInterceptorFn, HttpInterceptor, HttpRequest,
-  HttpHandlerFn} from '@angular/common/http';
+  HttpHandlerFn,
+  HttpHandler,
+  HttpErrorResponse,
+  HttpStatusCode} from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { EMPTY, catchError, throwError } from 'rxjs';
 
 // export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
@@ -13,6 +18,16 @@ export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) 
   const authReq = req.clone({
     setHeaders: { Authorization: 'myAuthToken' }
     });
+    const authService = inject(AuthService);
     
-  return next(authReq);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === HttpStatusCode.Unauthorized) {
+      authService.logout();
+      return EMPTY;
+      } else {
+      return throwError(() => error);
+      }
+      })
+  );
 }
